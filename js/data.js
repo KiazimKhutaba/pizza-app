e9a511f360d48fd9f41a3904759deeb3e3286bd8
Kiazim Khutaba <github.com/KiazimKhutaba>
2020-08-19 20:24:08 +0300

    added new components, basic screen switching, localstorage support

--- js/data.js
+++ js/data.js
@@ -1,6 +1,7 @@
 
 const pizzasList = [
     {
+        id: 1,
         name:  'Greek Pizza',
         image: 'https://www.cookingclassy.com/wp-content/uploads/2013/10/greek-pizza2+srgb.-450x500.jpg',
         desc: 'Pizza just got a delicious upgrade! This is loaded with fresh veggies and a satisfying greek flavor.',
@@ -9,6 +10,7 @@ const pizzasList = [
         currency: '$'
     },
     {
+        id: 2,
         name: 'Pizza Margherita',
         image: 'https://onlineculinaryschool.net/wp-content/uploads/2018/10/online_culinary_school_pizza_margherita-1-1024x576.jpg',
         desc: 'This classic pizza Margherita, is a pizza in its most basic, but nonetheless delicious, form: dough, pizza sauce, fresh basil, mozzarella and Parmesan cheeses.',
@@ -17,6 +19,7 @@ const pizzasList = [
         currency: '$'
     },
     {
+        id: 3,
         name: 'Pizza Marinara',
         image: 'https://www.pizzarecipe.org/wp-content/uploads/2019/01/Pizza-Marinara-2000x1500.jpg',
         desc: 'Pizza marinara is classic Neapolitan pizza. There’s no cheese, so it’s all about the tomatoes, garnished simply with garlic and oregano. Buon appetito!',
@@ -25,6 +28,7 @@ const pizzasList = [
         currency: '$'
     },
     {
+        id: 4,
         name: 'Pizza Carbonara',
         image: 'https://www.mutherfudger.co.uk/wp-content/uploads/2013/03/chicken-carbonara-pizza-300x225.jpg',
         desc: 'This easy luxe pizza is ready in a flash.',
@@ -33,6 +37,7 @@ const pizzasList = [
         currency: '$'
     },
     {
+        id: 5,
         name:  'Pizza Prosciutto crudo',
         image: 'https://straus.s3.amazonaws.com/media/products2/21920_big.jpg?version=1576470756',
         desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio odio, earum molestiae ',
@@ -41,6 +46,7 @@ const pizzasList = [
         currency: '$'
     },
     {
+        id: 6,
         name: 'Pizza Montanara',
         image: 'https://res.cloudinary.com/hksqkdlah/image/upload/ar_1:1,c_fill,dpr_2.0,f_auto,fl_lossy.progressive.strip_profile,g_faces:auto,q_auto:low,w_344/24595_sfs-fried-pizza-montanara-35',
         desc: 'Fried pizza sounds like novelty food. But when it’s properly done, this centuries-old tradition produces a pie that is crisp, chewy, and far from greasy.',
@@ -49,6 +55,7 @@ const pizzasList = [
         currency: '$'
     },
     {
+        id: 7,
         name: 'Pizza Romana',
         image: 'https://www.refrigeratedfrozenfood.com/ext/resources/NEW_RD_Website/DefaultImages/default-pizza.jpg?1430942592',
         desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, possimus',
@@ -57,6 +64,7 @@ const pizzasList = [
         currency: '$'
     },
     {
+        id: 8,
         name: 'Pizza Americana',
         image: 'https://www.silviocicchi.com/pizzachef/wp-content/uploads/2015/02/a-evid.jpg',
         desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, possimus',
