b7f2f5331d9d7224837eb39f6c0e498b750a63c6
Kiazim Khutaba <github.com/KiazimKhutaba>
2020-08-19 10:49:39 +0300

    added some real pizza data

--- js/data.js
+++ js/data.js
@@ -1,59 +1,59 @@
 
 const pizzasList = [
     {
-        name:  '<div>Pizza 1</div>',
-        image: 'https://source.unsplash.com/250x180/?pizza',
+        name:  'Greek Pizza',
+        image: 'https://www.cookingclassy.com/wp-content/uploads/2013/10/greek-pizza2+srgb.-450x500.jpg',
+        desc: 'Pizza just got a delicious upgrade! This is loaded with fresh veggies and a satisfying greek flavor.',
+        price: 34.15,
+        currency: '$'
+    },
+    {
+        name: 'Pizza Margherita',
+        image: 'https://onlineculinaryschool.net/wp-content/uploads/2018/10/online_culinary_school_pizza_margherita-1-1024x576.jpg',
+        desc: 'This classic pizza Margherita, is a pizza in its most basic, but nonetheless delicious, form: dough, pizza sauce, fresh basil, mozzarella and Parmesan cheeses.',
+        price: 29.95,
+        currency: '$'
+    },
+    {
+        name: 'Pizza Marinara',
+        image: 'https://www.pizzarecipe.org/wp-content/uploads/2019/01/Pizza-Marinara-2000x1500.jpg',
+        desc: 'Pizza marinara is classic Neapolitan pizza. There’s no cheese, so it’s all about the tomatoes, garnished simply with garlic and oregano. Buon appetito!',
+        price: 28.95,
+        currency: '$'
+    },
+    {
+        name: 'Pizza Carbonara',
+        image: 'https://www.mutherfudger.co.uk/wp-content/uploads/2013/03/chicken-carbonara-pizza-300x225.jpg',
+        desc: 'This easy luxe pizza is ready in a flash.',
+        price: 12.85,
+        currency: '$'
+    },
+    {
+        name:  'Pizza Prosciutto crudo',
+        image: 'https://straus.s3.amazonaws.com/media/products2/21920_big.jpg?version=1576470756',
         desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, possimus',
         price: 34.15,
         currency: '$'
     },
     {
-        name: 'Pizza 2',
-        image: 'https://source.unsplash.com/250x180/?pizza',
-        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, possimus',
+        name: 'Pizza Montanara',
+        image: 'https://res.cloudinary.com/hksqkdlah/image/upload/ar_1:1,c_fill,dpr_2.0,f_auto,fl_lossy.progressive.strip_profile,g_faces:auto,q_auto:low,w_344/24595_sfs-fried-pizza-montanara-35',
+        desc: 'Fried pizza sounds like novelty food. But when it’s properly done, this centuries-old tradition produces a pie that is crisp, chewy, and far from greasy.',
         price: 29.95,
         currency: '$'
     },
     {
-        name: 'Pizza 3',
-        image: 'https://source.unsplash.com/250x180/?pizza',
+        name: 'Pizza Romana',
+        image: 'https://www.refrigeratedfrozenfood.com/ext/resources/NEW_RD_Website/DefaultImages/default-pizza.jpg?1430942592',
         desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, possimus',
         price: 28.95,
         currency: '$'
     },
     {
-        name: 'Pizza 4',
-        image: 'https://source.unsplash.com/250x180/?pizza',
+        name: 'Pizza Americana',
+        image: 'https://www.silviocicchi.com/pizzachef/wp-content/uploads/2015/02/a-evid.jpg',
         desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, possimus',
-        price: 41.85,
-        currency: '$'
-    },
-    {
-        name:  '<div>Pizza 1</div>',
-        image: 'https://source.unsplash.com/250x180/?pizza',
-        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, possimus',
-        price: 34.15,
-        currency: '$'
-    },
-    {
-        name: 'Pizza 2',
-        image: 'https://source.unsplash.com/250x180/?pizza',
-        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, possimus',
-        price: 29.95,
-        currency: '$'
-    },
-    {
-        name: 'Pizza 3',
-        image: 'https://source.unsplash.com/250x180/?pizza',
-        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, possimus',
-        price: 28.95,
-        currency: '$'
-    },
-    {
-        name: 'Pizza 4',
-        image: 'https://source.unsplash.com/250x180/?pizza',
-        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, possimus',
-        price: 41.85,
+        price: 9.95,
         currency: '$'
     },
     
