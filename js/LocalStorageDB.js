fd9b2b1ca9e96efe680b12667924c1fc38a27a10
Kiazim Khutaba <github.com/KiazimKhutaba>
2020-08-19 14:43:09 +0300

    added LocalStorageDB class

---
+++ js/LocalStorageDB.js
@@ -0,0 +1,84 @@
+
+class LocalStorageDB
+{
+
+    constructor(dbPrefix) {
+
+        //console.warn('LocalStorageDb object created');
+        this.dbPrefix = dbPrefix + '@';
+    }
+
+    /**
+     * Insert data to localStorage
+     *
+     *
+     * @param {string} key
+     * @param {any} value
+     */
+    save(key, value) {
+
+        let _value = JSON.stringify(value);
+
+        localStorage.setItem(this.dbPrefix + key, _value);
+
+        return true;
+    }
+
+
+    /**
+     * Select data from localStorage
+     *
+     *
+     * @param {string} key object key id
+     *
+     * @returns {object|null}
+     */
+    fetch(key) {
+        let data = localStorage.getItem(this.dbPrefix + key);
+
+        if(data === null)
+            return false;
+
+        let _data = false;
+
+        try {
+
+            _data = JSON.parse(data);
+
+        } catch (e) {
+            console.error(e);
+        }
+
+        return _data;
+    }
+
+
+    delete(key) {
+        localStorage.removeItem(this.dbPrefix + key);
+    }
+
+
+    rowsCount() {
+        return localStorage.length;
+    }
+
+
+    drop() {
+
+        for( let i=0; i < this.rowsCount(); i++ ) {
+
+            let key = localStorage.key(i);
+
+            if( key.startsWith(this.dbPrefix) )
+                this.delete(key);
+        }
+    }
+
+
+    dropAll() {
+        localStorage.clear();
+    }
+
+}
+
+export default LocalStorageDB;
