6afac6d5016076683d724c269b16afd4610cbae7
Kiazim Khutaba <github.com/KiazimKhutaba>
2020-08-20 15:26:30 +0300

    some changes before class go to class components

--- app.js
+++ app.js
@@ -12,10 +12,18 @@ const APP_NAME = 'PizzaApp';
 
 /**
  * 
- * @param {Document} element 
+ * @param {HTMLElement} app 
+ * @param {Object} screens 
  */
 function Router(app, screens)
 {
+
+    /**
+     * 
+     * @param {string} component HTML markup
+     */
+    const render = (component) => app.innerHTML = component;
+
 
     const hashChangeHandler = (e) => {
 
@@ -29,7 +37,7 @@ function Router(app, screens)
 
         if( hash.startsWith('#!') ) 
         {
-            // stop propagination
+            // stops default actiom
             e.preventDefault();
 
             const Component = screens[hash.slice(2)];
@@ -37,10 +45,10 @@ function Router(app, screens)
             //log(screen);
 
             if( !Component ) {
-                app.innerHTML = NotFound(hash.slice(2));
+                render(NotFound(hash.slice(2)));
             }
             else {
-                app.innerHTML = Component();
+                render(Component());
             }
 
             
@@ -85,19 +93,61 @@ function Router(app, screens)
 function main() 
 {
     let app = el('#app');
-    let root = el('.products-list__items');
+    
+
+    class TestView
+    {
+
+        constructor() {
+            log('Test object created!!!');
+        }
+
+
+        someMethod(e) {
+            alert(e.target.dataset.name)
+        }
+
+
+        eventHandler() {
+            el('#jKsnsn').addEventListener('click', this.someMethod);
+        }
+
+
+        render = () => (
+
+            /* html */
+            `
+                <div id="jKsnsn">
+                    <h4 data-name="test1">Test Closure Component 1</h4>
+                    <h4 data-name="test2">Test Closure Component 2</h4>
+                    <h4 data-name="test3">Test Closure Component 3</h4>
+                </div>
+            `
+        )
+    }
+
+    const obj = new TestView();
+    app.innerHTML = obj.render();
+    obj.eventHandler();
+    
 
 
     const screens = {
         'main': ProductsList,
         'cart': CartView, 
-        'login': LoginView
+        'login': LoginView,
+        'test': TestView
     };
 
 
 
-    Router(app, screens);
+   
 
+    //Router(app, screens);
+    //let root = el('.products-list__items');
+
+
+   
 
     //app.innerHTML = ProductsList();
 
@@ -105,51 +155,51 @@ function main()
      * 
      * Product card actions
      */
-    root.addEventListener('click', (e) => {
+    // root.addEventListener('click', (e) => {
     
-        const Actions = {
-            PRODUCT_ADD:    'product.add',
-            PRODUCT_REMOVE: 'product.remove',
-            CART_ADD:       'card.add'
-        };
+    //     const Actions = {
+    //         PRODUCT_ADD:    'product.add',
+    //         PRODUCT_REMOVE: 'product.remove',
+    //         CART_ADD:       'card.add'
+    //     };
 
-        //log('Element dataset: ', e.target.className);
+    //     //log('Element dataset: ', e.target.className);
 
-        // handle action buttons events
-        if( e.target.className.startsWith('actions__') )
-        {
-            let product = e.target.closest('div.product').dataset;
-            let action  = e.target && e.target.dataset.action; // dataset can be empty
-            let productCount = el('#product-count-' + product.id);
+    //     // handle action buttons events
+    //     if( e.target.className.startsWith('actions__') )
+    //     {
+    //         let product = e.target.closest('div.product').dataset;
+    //         let action  = e.target && e.target.dataset.action; // dataset can be empty
+    //         let productCount = el('#product-count-' + product.id);
 
 
-            const db = new LocalStorageDB(APP_NAME);
-            const cart = new Cart(db);
+    //         const db = new LocalStorageDB(APP_NAME);
+    //         const cart = new Cart(db);
         
 
-            //log('action', action, ', product.id', product.id, ', product.count', productCount.textContent)
+    //         //log('action', action, ', product.id', product.id, ', product.count', productCount.textContent)
 
-            if( Actions.PRODUCT_ADD === action ) {
-                productCount.textContent = +productCount.textContent + 1;
-                cart.add(product.id, product);
-            }
+    //         if( Actions.PRODUCT_ADD === action ) {
+    //             productCount.textContent = +productCount.textContent + 1;
+    //             cart.add(product.id, product);
+    //         }
 
 
-            if( Actions.PRODUCT_REMOVE === action ) {
+    //         if( Actions.PRODUCT_REMOVE === action ) {
 
-                let count = +productCount.textContent;
-                if( count === 0 ) return;
+    //             let count = +productCount.textContent;
+    //             if( count === 0 ) return;
 
-                productCount.textContent = +productCount.textContent - 1;
-                cart.remove(product.id);
-            }
+    //             productCount.textContent = +productCount.textContent - 1;
+    //             cart.remove(product.id);
+    //         }
 
-            if( Actions.CART_ADD === action ) {
-                e.target.disabled = true;
-                e.target.style.background = 'grey';
-            }
-        }
-    })
+    //         if( Actions.CART_ADD === action ) {
+    //             e.target.disabled = true;
+    //             e.target.style.background = 'grey';
+    //         }
+    //     }
+    // })
 
 }
 
--- js/Container/ProductsList.js
+++ js/Container/ProductsList.js
@@ -9,6 +9,7 @@ function ProductsList()
     pizzasList.forEach(pizza => items += ProductView(pizza))
 
 
+
     let list = /* html */`
         <div class="products-list">
             <h2 class="products-list__title">Most Popular Pizzas</h2>
