75b62e3bfb8875e335dfe5b1ce253d50d4501657
Kiazim Khutaba <github.com/KiazimKhutaba>
2020-08-25 11:51:29 +0300

    added test api route

--- config/routes.php
+++ config/routes.php
@@ -8,14 +8,16 @@ use PizzaApp\Controllers\EntryPointController;
 
 return function (App $app) {
 
-    $app->get('/', EntryPointController::class . ':index' );
-
     // index
+    $app->group('', function(RouteCollectorProxy $group) {
+        
+        $group->get ('/',  EntryPointController::class . ':index' );
+    });
+
+    // api
     $app->group('/api/v1', function(RouteCollectorProxy $group) {
         
         $group->get('/products', ProductsController::class . ':all' );
-
-        //$group->get('/post/{url}', PostController::class . ':index');
     });
 
 
